8006998a26716b61a167057a2f5ea5e606373b86
leahnp613 <leahnagarpowers@gmail.com>
2023-03-21 11:55:37 -0400

    added constants and init function

--- js/app.js
+++ js/app.js
@@ -4,10 +4,29 @@
 
 /*----------------------------Variables------------------------------*/
 
+let secretNum, guessList, isWinner
 
 /*---------------------------Cached Element References-----------------*/
 
-/*--------------------------Event Listeners----------------------------*/
+const form = document.querySelector("form")
+const guessInput = document.querySelector("#guess-input")
+const guessesEl = document.querySelector("#prev-guesses")
+const messageEl = document.querySelector("#reset-button")
+const prevGuessMsg = document.querySelector("#prev-guesses-msg")
 
+/*--------------------------Event Listeners----------------------------*/
+form.addEventListener("reset", init)
 
 /*-------------------------Functions------------------------------------*/
+init();
+
+function init(){
+    guessesEl.innerText=""
+    messageEl.innerText= "Please enter a number from 1 to 100"
+    resetBtn.setAttribute("hidden", true)
+    prevGuessMsg.innerText= ""
+    guessList =[]
+    isWinner = false
+    secretNum = Math.floor(Math.random() *100 +1)
+}
+
